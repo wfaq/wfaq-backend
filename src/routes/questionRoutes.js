6f9ec22dcde14f60459f375f1abb2a31af15efa0
Douglas Benitez <douglasfazion@gmail.com>
2019-11-25 19:40:35 -0300

    Adicionados votos nas respostas(up and down)

--- src/routes/questionRoutes.js
+++ src/routes/questionRoutes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 
 const QuestionController = require("../controllers/questionController");
 const AnswerController = require("../controllers/AnswerController");
+const AnswerVotesController = require("../controllers/AnswerVotesController");
 
 router.get("/", QuestionController.index);
 router.post("/", QuestionController.create);
@@ -11,5 +12,8 @@ router.delete("/:id", QuestionController.destroy);
 router.post("/:id/answers", AnswerController.store);
 router.put("/:id/answers/:answerId", AnswerController.update);
 router.delete("/:id/answers/:answerId", AnswerController.destroy);
+
+router.post("/:id/answers/:answerId/up", AnswerVotesController.up);
+router.post("/:id/answers/:answerId/down", AnswerVotesController.down);
 
 module.exports = router;
