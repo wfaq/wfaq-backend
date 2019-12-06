e9661dc8dcf68124542401be01430a56186ceb82
Douglas Benitez <douglasfazion@gmail.com>
2019-12-06 07:20:29 -0300

    Pesquisa pelo titulo ignorando case sensitive

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -2,8 +2,10 @@ const question = require("../models/questions");
 
 module.exports = {
   async index(req, res) {
- // const { search } = req.body;
-  const questions = await question.find({"title":{ $regex: `.*${req.body.search}.*`}});
+    console.log(req.query.search);
+    const questions = await question.find({
+      title: { $regex: `.*${req.query.search}.*`, $options: "i" }
+    });
     return res.status(200).json(questions);
-  },
-} 
+  }
+};
