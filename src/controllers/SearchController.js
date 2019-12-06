1583b186b19303ee000e6f249db42593a552a127
Douglas Benitez <douglasfazion@gmail.com>
2019-12-06 07:45:57 -0300

    Pesquisa considera o titulo e a pergunta para trazer os resultados

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -4,7 +4,10 @@ module.exports = {
   async index(req, res) {
     console.log(req.query.search);
     const questions = await question.find({
-      title: { $regex: `.*${req.query.search}.*`, $options: "i" }
+      $or: [
+        { title: { $regex: `.*${req.query.search}.*`, $options: "i" } },
+        { question: { $regex: `.*${req.query.search}.*`, $options: "i" } }
+      ]
     });
     return res.status(200).json(questions);
   }
