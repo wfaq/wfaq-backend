5b1c855c7b762e7970f1b3463ccc0728b6e9c23f
Douglas Benitez <douglasfazion@gmail.com>
2019-12-06 07:50:19 -0300

    Adicionada verificação do envio da pesquisa

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -2,12 +2,19 @@ const question = require("../models/questions");
 
 module.exports = {
   async index(req, res) {
+    const { search } = req.query;
+
+    if (!search) {
+      return res.status(400).json({ message: "Empty Question" });
+    }
+
     const questions = await question.find({
       $or: [
-        { title: { $regex: `.*${req.query.search}.*`, $options: "i" } },
-        { question: { $regex: `.*${req.query.search}.*`, $options: "i" } }
+        { title: { $regex: `.*${search}.*`, $options: "i" } },
+        { question: { $regex: `.*${search}.*`, $options: "i" } }
       ]
     });
+
     return res.status(200).json(questions);
   }
 };
